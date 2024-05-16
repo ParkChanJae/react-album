bdd836294fcae9dbb7236f2a7fda5105b67aee47
ParkChanJae <ekzm2302@naver.com>
2024-05-16 14:39:39 +0900

    Dialog

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -14,11 +14,11 @@ import DetailDialog from '@/components/common/dialog/DetailDialog';
 
 function index() {
     const imgSelector = useRecoilValue(imageData);
-    const [imgData, setImgData] = useState<CardDTO[]>([]);
+    const [imgData, setImgData] = useState<CardDTO>();
     const [open, setOpen] = useState<boolean>(false); // 이미지 상세 다이얼로그
 
     const CARD_LIST = imgSelector.data.results.map((card: CardDTO) => {
-        return <Card data={card} key={card.id} handleDialog={setOpen} />;
+        return <Card data={card} key={card.id} handleDialog={setOpen} handleSetData={setImgData} />;
     });
 
     return (
@@ -44,7 +44,7 @@ function index() {
             {/* 공통 푸터 UI 부분 */}
             <CommonFooter />
 
-            {open && <DetailDialog />}
+            {open && <DetailDialog data={imgData} handleDialog={setOpen} />}
         </div>
     );
 }
